0cf891760bc48449ac38d6eabce33a4a67e02a38
julieandersson <juan2301@student.miun.se>
2024-09-19 11:17:48 +0200

    läser in httpclient, hämtar kurser genom getcourses och returnerar datan

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,12 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  // private url (egenskap), typ string
   private url: string = "https://webbutveckling.miun.se/files/ramschema_ht23.json";
 
-  
-  constructor() { }
+
+  constructor(private http: HttpClient) { } // Läser in HttpClient
+
+  getCourses(): Observable<Course[]> { // Hämtar kurser genom att ta emot en array av kurser enligt interfacet
+
+    return this.http.get<Course[]>(this.url); // returnerar data från url från getCourses
+  }
 }
