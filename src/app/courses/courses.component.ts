de7652e5525f2ee8930a9a4ed49efb763bde965e
julieandersson <juan2301@student.miun.se>
2024-09-19 11:39:17 +0200

    skapar kurslista, konstruktor och lite html-kod med ngFor för att se så att datan hämtas och skrivs ut på webbplatsen

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
+
+  // Kurslista från interfacet, tar en tom array
+  courseList: Course[] = [];
+
+  // Konstruktor för import av kurser
+  constructor(private coursesservice: CoursesService) {}
+
+  // Kör ngOnInit-metoden när applikationen startar
+  ngOnInit() {
+    this.coursesservice.getCourses().subscribe(data => {
+      this.courseList = data; // Lagrar datan i courselist
+    })
+  }
 
 }
