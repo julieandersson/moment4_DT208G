cfcffda1b53af8dd9b2c4849de89c2e2634c17f6
julieandersson <juan2301@student.miun.se>
2024-09-19 12:11:27 +0200

    implementerar mer funktionalitet för listan med kurser

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
@@ -14,6 +15,13 @@ export class CoursesComponent {
 
   // Kurslista från interfacet, tar en tom array
   courseList: Course[] = [];
+  // filtrerad kurslista baserat på användarens sökfråga
+  filteredCourses: Course[] = [];
+  // variabel för söktexten från inmatning
+  searchText: string = '';
+  // sortering av kurslistan, code och stigande ordning
+  sortField: string = 'code';
+  sortDirection: 'asc' | 'desc' = 'asc';
 
   // Konstruktor för import av kurser
   constructor(private coursesservice: CoursesService) {}
